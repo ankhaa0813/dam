64ffaa84d5283a12d04d66d04782bb5088760e7a
ankhaa <ankhbayr0813@gmail.com>
2020-07-31 18:57:07 +0800

    First commit

---
+++ flight/views.py
@@ -0,0 +1,33 @@
+from django import forms
+from django.http import HttpResponseBadRequest, HttpResponseRedirect, Http404
+from django.shortcuts import render
+from django.urls import reverse
+
+from .models import Flight, Passenger
+# Create your views here.
+def index(request):
+    return render(request, "flight/index.html", {
+       "flight": Flight.objects.all()
+    })
+def flight(request, flight_id):     
+   flight = Flight.objects.get(id = flight_id)
+   return render(request, "flight/flight.html", {
+      "flight": flight,
+      "passenger": flight.passengers.all(),
+      "non_passenger": Passenger.objects.exclude(flights=flight).all()
+   })
+
+
+def book(request, flight_id):
+    if request.method == "POST":
+        try:
+            passenger = Passenger.objects.get(pk=int(request.POST["passenger"]))
+            flight = Flight.objects.get(pk=flight_id)
+        except KeyError:
+            return HttpResponseBadRequest("Bad Request: no flight chosen")
+        except Flight.DoesNotExist:
+            return HttpResponseBadRequest("Bad Request: flight does not exist")
+        except Passenger.DoesNotExist:
+            return HttpResponseBadRequest("Bad Request: passenger does not exist")
+        passenger.flights.add(flight)
+        return HttpResponseRedirect(reverse("flight", args=(flight.id,)))
